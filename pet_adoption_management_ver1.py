2264ee7bdd7ab48ef0e3a501f4a2bc6684045b22
Eliza Lazar <elizalazar.ca@gmail.com>
2025-12-22 21:50:57 -0700

    Added code to show_menu(), add_pet(), list_pets(), adopt_pets(). Also updated Class Pet and main().

--- pet_adoption_management_ver1.py
+++ pet_adoption_management_ver1.py
@@ -31,42 +31,87 @@ class Pet:
         self.age = int(age)
         self.adopted = bool(adopted)
     
+    def __str__(self):
+        return f"Name: {self.name}\n Type: {self.type}\n Age: {self.age}\n Status: {self.adopted}\n Pet ID: {self.pet_id}0000\n"
+    
 def show_menu():
     """
 
     """
-    print()
+    invalid = True;
+    print("Welcome to Pet Adoption Centre!\n")
+    print("How can we help you today?")
+    print("1. View adoptable pets")
+    print("2. Add new pet to system")
+    print("3. Adopt a pet")
+    print("4. Exit application\n")
+    choice = int(input("Please submit your choice: "))
+    while (invalid):
+        if (choice == 1):
+            invalid = False
+            list_pets()
+        elif (choice == 2):
+            invalid = False
+            add_pet()
+        elif (choice == 3):
+            invalid = False
+            adopt_pet()
+        elif (choice == 4):
+            invalid = False
+            print("Thank you for visiting! Have a nice day :)\n")
+            exit()
+        else:
+            choice = int(input("Sorry, we didn't quite get that. Please submit your choice: "))
 
 def add_pet():
     """
 
     """
     global next_pet
-    print("Paperwork for New Arrivals:\n")
+    print("\nPaperwork for New Arrivals:\n")
     name = input("Pet name: ")
     type = input("Pet Type: ")
     age = input("Pet age: ")
     pet = Pet(next_pet, name, type, age, False)
     pets.append(pet)
     next_pet += 1
+    show_menu()
 
 def list_pets():
     """
 
     """
-    print()
+    print("\nCurrent Adoptables:\n")
+    global next_pet
+    global pets
+    invalid = True
+    for pet in pets:
+        print(pet)
+    print("What would you like to do?")
+    print("1. Go back to menu")
+    print("2. View specific pet (Under construction)")
+    choice = int(input("What would you like to do? "))
+    while (invalid):
+        if (choice == 1):
+            invalid = False
+            show_menu()
+        elif (choice == 2):
+            choice = int(input("Sorry, that feature is currently unavailable. What would you like to do? "))
+        else:
+            choice = int(input("Sorry, we didn't quite get that. What would you like to do? "))
+
 
 def adopt_pet():
     """
 
     """
-    print()
+    print("Adopt\n")
 
 def main():
     """
 
     """
-    add_pet()
+    show_menu()
 
 if __name__ == "__main__":
     main()
